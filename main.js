b687962a448a56f6104e73640abfdff0967f0a08
xuan <sakana9826@gmail.com>
2024-01-26 09:53:49 +0800

    初始化提交

---
+++ main.js
@@ -0,0 +1,31 @@
+const { app, BrowserWindow } = require("electron");
+const path = require("path");
+
+let mainWindow;
+
+function createWindow() {
+  mainWindow = new BrowserWindow({
+    width: 800,
+    height: 600,
+    webPreferences: {
+      nodeIntegration: true,
+      enableRemoteModule: true, // If you're using Electron v12 or later
+    },
+  });
+
+  mainWindow.loadFile("index.html");
+
+  mainWindow.on("closed", function () {
+    mainWindow = null;
+  });
+}
+
+app.whenReady().then(createWindow);
+
+app.on("window-all-closed", function () {
+  if (process.platform !== "darwin") app.quit();
+});
+
+app.on("activate", function () {
+  if (mainWindow === null) createWindow();
+});
